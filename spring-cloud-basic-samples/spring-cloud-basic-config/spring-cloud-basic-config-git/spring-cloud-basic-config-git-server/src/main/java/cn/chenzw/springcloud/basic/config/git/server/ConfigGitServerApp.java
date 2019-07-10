dd7d1110ccff203ea638b6c0825f094bb3d200bc
chenzw <656469722@qq.com>
2019-07-10 21:25:23 +0800

    initialization

--- spring-cloud-basic-samples/spring-cloud-basic-config/spring-cloud-basic-config-git/spring-cloud-basic-config-git-server/src/main/java/cn/chenzw/springcloud/basic/config/git/server/ConfigGitServerApp.java
+++ spring-cloud-basic-samples/spring-cloud-basic-config/spring-cloud-basic-config-git/spring-cloud-basic-config-git-server/src/main/java/cn/chenzw/springcloud/basic/config/git/server/ConfigGitServerApp.java
@@ -1,16 +1,14 @@
-package cn.chenzw.springcloud.basic;
+package cn.chenzw.springcloud.basic.config.git.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaServer
-public class ConfigServerApp {
+public class ConfigGitServerApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApp.class, args);
+        SpringApplication.run(ConfigGitServerApp.class, args);
     }
 }
