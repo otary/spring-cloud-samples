03ce170c3a2d8da47aa5dc168adc8cefc49f67ae
chenzw <chenzw@ffcs.cn>
2019-07-11 16:34:43 +0800

    initialization

--- spring-cloud-basic-samples/spring-cloud-basic-sleuth-zipkin/startup.cmd
+++ spring-cloud-basic-samples/spring-cloud-basic-sleuth-zipkin/startup.cmd
@@ -1,7 +1,5 @@
 @echo off
 
-set ZIPKIN_SERVER_APP=zipkin-server
-set ZIPKIN_PRODUCER_APP=spring-cloud-basic-zipkin-producer
-set ZIPKIN_CONSUMER_APP=spring-cloud-basic-zipkin-consumer
-
-mvn package & start %ZIPKIN_SERVER_APP%\startServer.cmd & start %ZIPKIN_PRODUCER_APP%\startup.cmd & start %ZIPKIN_CONSUMER_APP%\startup.cmd
+start zipkin-server\startServer.cmd
+start cmd /k "cd spring-cloud-basic-zipkin-producer & mvn spring-boot:run"
+start cmd /k "cd spring-cloud-basic-zipkin-consumer & mvn spring-boot:run"
