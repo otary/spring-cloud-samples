fb57c7f37ca11895f01cd84a97d550e13fa61b26
chenzw <chenzw@ffcs.cn>
2019-07-11 17:13:57 +0800

    initialization

--- spring-cloud-basic-samples/spring-cloud-basic-zuul/startup.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set APP_FILE=%~dp0%/target/spring-cloud-basic-zuul-0.0.1-SNAPSHOT.jar
-
-if not exist %APP_FILE% (
-    mvn install & java -jar %APP_FILE%
-)else (
-   java -jar %APP_FILE%
-)
--- spring-cloud-basic-samples/startup.cmd
+++ spring-cloud-basic-samples/startup.cmd
@@ -1,10 +1,8 @@
 @echo off
 
-set EUREKA_SERVER_APP=spring-cloud-basic-eureka\spring-cloud-basic-eureka-server
-set EUREKA_CLIENT_APP=spring-cloud-basic-eureka\spring-cloud-basic-eureka-client
-set RIBBON_APP=spring-cloud-basic-discovery\spring-cloud-basic-ribbon
-set FEIGN_APP=spring-cloud-basic-discovery\spring-cloud-basic-feign
-set ZUUL_APP=spring-cloud-basic-zuul
 
-mvn package & start %EUREKA_SERVER_APP%\startup.cmd & start %EUREKA_CLIENT_APP%\startup.cmd & start %RIBBON_APP%\startup.cmd & start %FEIGN_APP%\startup.cmd & start %ZUUL_APP%\startup.cmd
-
+start cmd /k "cd spring-cloud-basic-eureka\spring-cloud-basic-eureka-server & mvn spring-boot:run"
+start cmd /k "cd spring-cloud-basic-eureka\spring-cloud-basic-eureka-client & mvn spring-boot:run"
+start cmd /k "cd spring-cloud-basic-discovery\spring-cloud-basic-ribbon & mvn spring-boot:run"
+start cmd /k "cd spring-cloud-basic-discovery\spring-cloud-basic-feign & mvn spring-boot:run"
+start cmd /k "cd spring-cloud-basic-zuul & mvn spring-boot:run"
